8918d0be591632f47c67ebc9b9a425c516446cad
zhoushuang <xwp_fullstack@163.com>
2018-06-27 23:38:09 +0800

    项目目录

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,7 +1,12 @@
 from . import new_blue
-from flask import session,render_template
+from flask import session,render_template,current_app
 
 @new_blue.route('/')
 def index():
     session['name'] = '2018'
     return render_template('./news/index.html')
+
+
+@new_blue.route('/favicon.ico')
+def favicon():
+    return current_app.send_static_file('news/favicon.ico')
--- manage.py
+++ manage.py
@@ -1,13 +1,14 @@
 from flask_script import Manager
 from info import create_app,db
 from flask_migrate import MigrateCommand,Migrate
-
+from info import models
 
 
 app = create_app('develop')
 manage = Manager(app)
 Migrate(app,db)
 manage.add_command('db', MigrateCommand)
+
 
 
 if __name__ == '__main__':
